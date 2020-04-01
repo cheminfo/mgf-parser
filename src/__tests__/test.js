fd0a88b795e689ac554984ed8fe415d975a51a6a
Océane Patiny <oceane@patiny.com>
2020-04-01 14:27:31 +0200

    core: wrote tests

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,7 +1,31 @@
-import { myModule } from '..';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-describe('test myModule', () => {
-  it('should return 42', () => {
-    expect(myModule()).toEqual(42);
+import { parse } from '..';
+
+let data = readFileSync(join(__dirname, '../../data/test.mgf'), 'utf8');
+let corruptedData = readFileSync(
+  join(__dirname, '../../data/corrupted.mgf'),
+  'utf8',
+);
+let otherEntryTypeData = readFileSync(
+  join(__dirname, '../../data/wrongEntryType.mgf'),
+  'utf8',
+);
+
+describe('index', () => {
+  it('parse test database', () => {
+    let result = parse(data);
+    expect(parse(data)).toHaveLength(1);
+    expect(result).toMatchSnapshot();
+    // console.log(result);
+  });
+  it('metadata corrupted (lacking "=")', () => {
+    expect(() => parse(corruptedData)).toThrow(
+      'Error: Data line number 7 could not be processed',
+    );
+  });
+  it('wrong entry type', () => {
+    expect(parse(otherEntryTypeData)[0].kind).toBe('PEPTIDES');
   });
 });
