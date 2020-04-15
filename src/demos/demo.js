080dee0b560b3b642a50a10316af1a91779a90eb
Océane Patiny <oceane@patiny.com>
2020-04-15 09:31:24 +0200

    chore (demo): add a demo script

---
+++ src/demos/demo.js
@@ -0,0 +1,20 @@
+// Command to run this file: `node -r esm demo.js | more`
+
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+import { parse } from '..';
+
+let data = readFileSync(
+  join(__dirname, '../../data/UNPD_ISDB_R_p04.mgf'),
+  'utf8',
+);
+
+const parsedData = parse(data, {
+  uniqueX: false,
+  normedY: false,
+});
+
+console.log('Number of entries:', parsedData.length);
+
+console.log(JSON.stringify(parsedData, undefined, 2));
