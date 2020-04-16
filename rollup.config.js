06d3f802ec3153a9079dbf185296a5ba60d04aa0
Luc Patiny <luc@patiny.com>
2020-04-16 09:02:00 +0200

    feat: add webbrowser build config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ export default {
   input: 'src/index.js',
   output: {
     format: 'cjs',
-    file: 'lib/index.js'
-  }
+    file: 'lib/index.js',
+  },
+  external: ['ml-array-normed', 'ml-array-xy-sort-x', 'ml-arrayxy-uniquex'],
 };
